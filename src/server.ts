2186b6ff214dc52a50cfe71fd4f4b5fbf3d17133
itz-kunal <kunal.wd067@gmail.com>
2026-07-11 15:16:59 +0530

    remove hard coded port

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import "dotenv/config"
 
 import { buildApp } from "./app.js";
 import { connectDb } from "./configs/db.config.js";
+
+const port = Number(process.env.PORT) || 3001;
 async function start() {
     const app = buildApp();
 
@@ -10,7 +12,7 @@ async function start() {
         await connectDb()
 
         const appInstance = await app.listen({
-            port: 3001,
+            port,
             host: "0.0.0.0"
         });
 
